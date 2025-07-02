c95074cf3632e7dcf30573060eccd176784c8d6c
Shilllo <kimsemen0402@mail.ru>
2025-07-02 16:34:50 +0300

    commit

--- src/ExportWalletButton.tsx
+++ src/ExportWalletButton.tsx
@@ -64,7 +64,7 @@ export function ExportWalletButton() {
                     Log in
                 </button>
             )}
-            {!isAuthenticated && (
+            {isAuthenticated && (
                 <button
                     onClick={handleExport}
                     disabled={!isAuthenticated || !embeddedWallet}
