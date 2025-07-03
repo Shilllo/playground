44b958a70dba098e09d90fc720441c570c0e9500
Shilllo <kimsemen0402@mail.ru>
2025-07-03 09:37:17 +0300

    Updates

--- assets/circle-CB8PrThM.js
+++
@@ -1,4 +0,0 @@
-import{a1 as i}from"./index-CpzFGo-0.js";import"./index-wZ2eiyL6.js";const o=i`<svg width="4" height="4" viewBox="0 0 4 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="2" cy="2" r="2" fill="white" fill-opacity="0.2"/>
-</svg>
-`;export{o as circleSvg};
---
+++ assets/transactions-DvyPRvHg.js
@@ -0,0 +1,16 @@
+import{i as f,a as m,x as a}from"./index-DYhQjUgh.js";import{c as p}from"./if-defined-BqzC0M4C.js";import"./index-CVbTQWjV.js";import"./index-D69xKrMU.js";import"./index-DAC5mI-K.js";import"./index-CpDoQhtO.js";import"./index-Cpwykh6w.js";import"./index-Bme316Ei.js";import"./index-wvff7hsQ.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var c=o.length-1;c>=0;c--)(l=o[c])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let s=class extends m{render(){return a`
+      <wui-flex flexDirection="column" .padding=${["0","m","m","m"]} gap="s">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};s.styles=d;s=u([p("w3m-transactions-view")],s);export{s as W3mTransactionsView};
