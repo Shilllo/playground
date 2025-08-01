41bf4e13b14485f7f37ca642c015a9f1678eca4e
Shilllo <kimsemen0402@mail.ru>
2025-08-01 18:10:53 +0300

    Updates

---
+++ assets/transactions-CtgB8MAg.js
@@ -0,0 +1,16 @@
+import{i as f,a as m,x as a}from"./index-DkMYBCsl.js";import{c as p}from"./if-defined-K_Wo30Uo.js";import"./index-DJqteKVO.js";import"./index-MKqArxOR.js";import"./index-PelFuicr.js";import"./index-CNxI7e-9.js";import"./index-C4GWV7du.js";import"./index-5YLc3WVi.js";import"./index-DyHBztUo.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var c=o.length-1;c>=0;c--)(l=o[c])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let s=class extends m{render(){return a`
+      <wui-flex flexDirection="column" .padding=${["0","m","m","m"]} gap="s">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};s.styles=d;s=u([p("w3m-transactions-view")],s);export{s as W3mTransactionsView};
